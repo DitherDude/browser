18a76d0c21c62648dae87567527146ce87b0b1d3
Dither Dude <choni05@outlook.com>
2025-06-28 22:26:33 +1000

    old get_elements()

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,5 @@
 use gtk::{Application, glib};
 use gtk::{ApplicationWindow, prelude::*};
-use regex::Regex;
-use std::collections::HashMap;
 use std::fs;
 
 const APP_ID: &str = "dither.browser";
@@ -14,19 +12,19 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
-fn build_ui(app: &Application, elements: &HashMap<String, String>) {
+fn build_ui(app: &Application, elements: &[(String, String)]) {
     let widgets = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .build();
     for element in elements {
         match element.0.as_str() {
             "button" => {
-                let settings = get_elements(element.1);
-                let label = stringify(settings.get("label")).unwrap_or("Edit Me!".to_string());
-                let margin_top = numerify(settings.get("margin_top")).unwrap_or(12);
-                let margin_bottom = numerify(settings.get("margin_bottom")).unwrap_or(12);
-                let margin_start = numerify(settings.get("margin_start")).unwrap_or(12);
-                let margin_end = numerify(settings.get("margin_end")).unwrap_or(12);
+                let settings = get_elements(&element.1);
+                let label = stringify("label", &settings, "Edit Me!");
+                let margin_top = numerify("margin_top", &settings, 12);
+                let margin_bottom = numerify("margin_bottom", &settings, 12);
+                let margin_start = numerify("margin_start", &settings, 12);
+                let margin_end = numerify("margin_end", &settings, 12);
                 let button = gtk::Button::builder()
                     .label(label)
                     .margin_top(margin_top)
@@ -44,67 +42,101 @@ fn build_ui(app: &Application, elements: &HashMap<String, String>) {
     }
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Some program ig lolz")
+        .title(stringify("title", elements, "Dither Browser"))
         .child(&widgets)
         .build();
     window.present();
 }
 
-fn get_elements(data: &str) -> HashMap<String, String> {
-    let re = Regex::new(r"\s*").unwrap();
-    let mut pushval = 0_i32;
-    let mut instring = None;
+fn get_elements(data: &str) -> Vec<(String, String)> {
+    let mut depth = 0_i32;
+    let mut datatype = 0u8;
     let mut escaping = false;
-    let mut value = false;
-    let mut result = HashMap::new();
-    let mut element = (String::new(), String::new());
+    let mut tailing = false;
+    let mut result = Vec::new();
+    let mut head = String::new();
     let mut subdata = String::new();
     for char in data.chars() {
-        subdata.push(char);
         let mut subescaping = false;
+        subdata.push(char);
+        if !char.is_numeric() && datatype == 3 {
+            datatype = 0;
+            depth -= 1;
+            if depth == 0 {
+                subdata.pop();
+                tailing = false;
+                result.push((head.trim().to_string(), subdata.trim().to_string()));
+                println!("Head: {}, Tail: {}", head.trim(), subdata.trim());
+                subdata = String::new();
+                head = String::new();
+            } else if depth < 0 {
+                return Vec::new();
+            }
+        }
         match char {
-            '{' => {
-                if !(instring.is_some() || escaping) {
-                    if !value {
+            '{' | ':' | '=' => {
+                if datatype == 0 && !escaping {
+                    if !tailing {
                         subdata.pop();
-                        subdata = re.replace(&subdata, "").to_string();
-                        element.0 = subdata;
+                        head = subdata.trim().to_string();
                         subdata = String::new();
                     }
-                    value = true;
-                    pushval += 1;
+                    tailing = true;
+                    depth += 1;
                 }
             }
             '}' => {
-                if !(instring.is_some() || escaping) {
-                    pushval -= 1;
-                    if pushval == 0 {
+                if datatype == 0 && !escaping {
+                    depth -= 1;
+                    if depth == 0 {
                         subdata.pop();
-                        element.1 = subdata;
+                        tailing = false;
+                        result.push((head.trim().to_string(), subdata.trim().to_string()));
+                        println!("Head: {}, Tail: {}", head.trim(), subdata.trim());
                         subdata = String::new();
-                        value = false;
-                        result.insert(element.0.clone(), element.1.clone());
-                        element = (String::new(), String::new());
-                    } else if pushval < 0 {
-                        return HashMap::new();
+                        head = String::new();
+                    } else if depth < 0 {
+                        return Vec::new();
                     }
                 }
             }
             '\\' => {
-                if instring.is_some() && !escaping {
+                if datatype != 0 && !escaping && depth < 2 {
                     subescaping = true;
+                    subdata.pop();
                 }
             }
             '\'' | '"' => {
-                if !value {
-                    return HashMap::new();
+                if !tailing {
+                    return Vec::new();
                 }
                 if !escaping {
-                    if instring.is_none() {
-                        instring = Some(char);
-                    } else if instring == Some(char) {
-                        instring = None;
+                    if datatype == 0 {
+                        depth += 1;
+                        datatype = if char == '\'' { 1 } else { 2 };
+                    } else if datatype == if char == '\'' { 1 } else { 2 } {
+                        datatype = 0;
+                        depth -= 1;
+                        if depth == 0 {
+                            tailing = false;
+                            result.push((head.trim().to_string(), subdata.trim().to_string()));
+                            println!("Head: {}, Tail: {}", head.trim(), subdata.trim());
+                            subdata = String::new();
+                            head = String::new();
+                        } else if depth < 0 {
+                            return Vec::new();
+                        }
                     }
+                }
+            }
+            ';' | ',' => {
+                if !escaping && datatype == 0 {
+                    subdata.pop();
+                }
+            }
+            '0'..='9' => {
+                if !escaping && datatype == 0 {
+                    datatype = 3;
                 }
             }
             _ => {}
@@ -114,24 +146,32 @@ fn get_elements(data: &str) -> HashMap<String, String> {
     result
 }
 
-fn stringify(input: Option<&String>) -> Option<String> {
+fn stringify(key: &str, array: &[(String, String)], fallback: &str) -> String {
+    let input = array
+        .iter()
+        .find(|&(skey, _)| skey == key)
+        .map(|(_, value)| value.clone());
     match input {
-        None => None,
+        None => fallback.to_string(),
         Some(x) => {
             if (x.starts_with('\"') && x.ends_with('\"'))
                 || (x.starts_with('\'') && x.ends_with('\''))
             {
-                Some(x[1..x.len() - 1].to_string())
+                x[1..x.len() - 1].to_string()
             } else {
-                None
+                fallback.to_string()
             }
         }
     }
 }
 
-fn numerify(input: Option<&String>) -> Option<i32> {
+fn numerify(key: &str, array: &[(String, String)], fallback: i32) -> i32 {
+    let input = array
+        .iter()
+        .find(|&(skey, _)| skey == key)
+        .map(|(_, value)| value.clone());
     match input {
-        None => None,
-        Some(x) => x.parse().ok(),
+        None => fallback,
+        Some(x) => x.parse().unwrap_or(fallback),
     }
 }
