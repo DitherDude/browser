15d53056880f957ef8db5ed0094a18dc5c152556
Dither Dude <choni05@outlook.com>
2025-08-16 21:56:20 +1000

    Implemented dynamic stacks for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,17 @@
-use async_std::path::{Path, PathBuf};
 use std::{
     cmp::Ordering,
+    collections::HashMap,
     env,
     fs::File,
     io::Read,
     net::{TcpListener, TcpStream},
-    path::Component,
+    path::{Component, Path, PathBuf},
+    sync::Arc,
 };
-use tracing::{debug, error, info, trace, warn};
+use tracing::{error, info, trace, warn};
 use utils::{receive_data, send_data, send_error, status, trace_subscription, version_compare};
 
 const DEFAULT_PORT: u16 = 6204;
-const SERVER_PTCLS: [&[u8; 5]; 4] = [b"HTML!", b"MRKDN", b"CRAWL", b"RWDTA"];
 #[async_std::main]
 async fn main() {
     let program_version: Vec<u32> = env!("CARGO_PKG_VERSION")
@@ -27,12 +27,14 @@ async fn main() {
     let mut verbose_level = 0u8;
     let args: Vec<String> = env::args().collect();
     let mut portstr = DEFAULT_PORT.to_string();
-    let mut pwd = String::new();
+    let mut pwd_str = String::new();
+    let mut stacksloc_str = String::new();
     for (i, arg) in args.iter().enumerate() {
         if arg.starts_with("--") {
             match arg.strip_prefix("--").unwrap_or_default() {
-                "directory" => pwd = args[i + 1].clone(),
+                "directory" => pwd_str = args[i + 1].clone(),
                 "port" => portstr = args[i + 1].clone(),
+                "stacks" => stacksloc_str = args[i + 1].clone(),
                 "verbose" => verbose_level += 1,
                 _ => panic!("Pre-init failure; unknown long-name argument: {arg}"),
             }
@@ -41,11 +43,15 @@ async fn main() {
             for char in arg.strip_prefix("-").unwrap_or_default().chars() {
                 match char {
                     'd' => {
-                        pwd = args[argindex + 1].clone();
+                        pwd_str = args[argindex + 1].clone();
                         argindex += 1;
                     }
                     'p' => {
                         portstr = args[argindex + 1].clone();
+                        argindex += 1;
+                    }
+                    's' => {
+                        stacksloc_str = args[argindex + 1].clone();
                         argindex += 1;
                     }
                     'v' => verbose_level += 1,
@@ -65,21 +71,58 @@ async fn main() {
             DEFAULT_PORT
         }
     };
-    if pwd.is_empty() {
-        pwd = match env::current_dir() {
-            Ok(pwd) => pwd.display().to_string(),
+    let pwd = if pwd_str.is_empty() {
+        match env::current_dir() {
+            Ok(pwd) => pwd,
             Err(e) => {
                 error!("Failed to get working directory directory: {}", e);
                 return;
             }
         }
     } else {
-        let directory = Path::new(&pwd);
-        if !directory.is_dir().await {
-            error!("Directory does not exist: {}", &pwd);
+        let directory = Path::new(&pwd_str);
+        if !directory.is_dir() {
+            error!("Directory does not exist: {}", &pwd_str);
             return;
         }
+        directory.to_path_buf()
+    };
+    let stacksloc = if stacksloc_str.is_empty() {
+        let mut dir = pwd.clone();
+        dir.push("stacks.txt");
+        if dir.is_file() {
+            dir
+        } else {
+            error!("Cannot find stacks file: {}", &stacksloc_str);
+            return;
+        }
+    } else {
+        let file = Path::new(&stacksloc_str);
+        if !file.is_file() {
+            error!("Cannot find specified stacks file: {}", &stacksloc_str);
+            return;
+        }
+        file.to_path_buf()
+    };
+    let mut stacksfile = match File::open(&stacksloc) {
+        Ok(file) => file,
+        Err(e) => {
+            error!("Failed to open stacks file: {}", e);
+            return;
+        }
+    };
+    let mut stacks = HashMap::new();
+    let mut lines = String::new();
+    if stacksfile.read_to_string(&mut lines).is_ok() {
+        for line in lines.lines() {
+            let (stack, loc) = line.split_at(5);
+            stacks.insert(stack.to_string(), loc.to_string());
+        }
+    } else {
+        error!("Failed to read stacks file: {}", &stacksloc_str);
+        return;
     }
+    let stacks = Arc::new(stacks);
     let listener = match TcpListener::bind("0.0.0.0:".to_owned() + &port.to_string()) {
         Ok(listener) => listener,
         Err(e) => {
@@ -99,16 +142,21 @@ async fn main() {
                     stream.peer_addr().unwrap().ip(),
                     stream.peer_addr().unwrap().port(),
                 );
-                let directory = pwd.clone();
+                let dir_clone = pwd.clone();
+                let stacks_ptr = Arc::clone(&stacks);
                 async_std::task::spawn(async move {
-                    handle_connection(stream, &directory).await;
+                    handle_connection(stream, &dir_clone, &stacks_ptr).await;
                 });
             }
         }
     }
 }
 
-async fn handle_connection(stream: TcpStream, directory: &str) {
+async fn handle_connection(
+    stream: TcpStream,
+    directory: &Path,
+    stacks: &Arc<HashMap<String, String>>,
+) {
     let program_version: Vec<u32> = env!("CARGO_PKG_VERSION")
         .split('.')
         .map(|f| match f.parse::<u32>() {
@@ -167,59 +215,49 @@ async fn handle_connection(stream: TcpStream, directory: &str) {
         }
     }
     let mut using_protocol = None;
+    let stacks = Arc::clone(stacks);
     for protocol in client_protocols {
-        if SERVER_PTCLS.contains(&&protocol) {
-            using_protocol = Some(protocol);
+        let stack = String::from_utf8_lossy(&protocol).to_string();
+        if let Some(protocol) = stacks.get(&stack) {
+            using_protocol = Some((stack, protocol.trim().to_string()));
             break;
         }
     }
-    if using_protocol.is_none() {
-        send_error(&stream, status::UNPROCESSABLE);
-        return;
-    }
-    let using_protocol = using_protocol.unwrap();
     let location = String::from_utf8_lossy(&data[0..]);
-    let protocol = using_protocol;
-    if &protocol == SERVER_PTCLS[0] {
-        html_content(&stream, &location, directory).await;
-    } else if &protocol == SERVER_PTCLS[1] {
-        markdown_content(&stream, &location, directory).await;
-    } else if &protocol == SERVER_PTCLS[2] {
-        crawl_content(&stream, &location, directory).await;
-    } else if &protocol == SERVER_PTCLS[3] {
-        raw_data_content(&stream, &location, directory).await;
-    } else {
-        error!("This path is unreachable!");
-    };
+    match using_protocol {
+        None => send_error(&stream, status::UNPROCESSABLE),
+        Some(protocol) => get_content(&stream, protocol, directory, &location),
+    }
 }
 
-async fn html_content(stream: &TcpStream, _destination: &str, _directory: &str) {
-    send_data(&status::TEST_NOT_IMPLEMENTED.to_le_bytes(), stream);
-}
-async fn markdown_content(stream: &TcpStream, destination: &str, directory: &str) {
-    let path = match pathcheck(destination, Path::new(directory)) {
-        Some(res) => res,
-        _ => {
-            send_error(stream, status::NOT_FOUND);
-            return;
+fn get_content(
+    stream: &TcpStream,
+    protocol: (String, String),
+    directory: &Path,
+    destination: &str,
+) {
+    let (stack, protocol) = protocol;
+    let mut payload = status::SUCCESS.to_le_bytes().to_vec();
+    payload.extend_from_slice(stack.as_bytes());
+    let file = if protocol.starts_with("/") {
+        match get_file(&protocol, directory) {
+            Some(content) => content,
+            None => {
+                send_error(stream, status::SHAT_THE_BED);
+                return;
+            }
+        }
+    } else {
+        let dest = format!("{destination}/{protocol}");
+        match get_file(&dest, directory) {
+            Some(content) => content,
+            None => {
+                send_error(stream, status::NOT_FOUND);
+                return;
+            }
         }
     };
-    if !path.is_dir().await {
-        send_error(stream, status::NOT_FOUND);
-        return;
-    }
-    let content = path.join("index.md");
-    if !content.is_file().await {
-        send_error(stream, status::NOT_FOUND);
-        return;
-    }
-    debug!(
-        "Client requested markdown content for path: {}",
-        &destination
-    );
-    let mut payload = status::SUCCESS.to_le_bytes().to_vec();
-    payload.extend_from_slice(SERVER_PTCLS[1]);
-    let filedump = File::open(&content);
+    let filedump = File::open(&file);
     match filedump {
         Ok(mut file) => {
             let mut buffer = Vec::new();
@@ -229,59 +267,29 @@ async fn markdown_content(stream: &TcpStream, destination: &str, directory: &str
                 }
                 Err(e) => {
                     warn!("Failed to read file: {}", e);
-                    send_error(stream, status::NOT_FOUND);
-                    return;
+                    send_error(stream, status::NOT_FOUND)
                 }
             }
         }
         Err(e) => {
             warn!("Failed to open file: {}", e);
-            send_error(stream, status::NOT_FOUND);
-            return;
+            send_error(stream, status::NOT_FOUND)
         }
     }
     send_data(&payload, stream);
 }
-async fn crawl_content(stream: &TcpStream, _destination: &str, _directory: &str) {
-    send_data(&status::TEST_NOT_IMPLEMENTED.to_le_bytes(), stream);
-}
-async fn raw_data_content(stream: &TcpStream, content: &str, directory: &str) {
-    let path = match pathcheck(content, Path::new(directory)) {
+
+fn get_file(subpath: &str, directory: &Path) -> Option<PathBuf> {
+    let path = match pathcheck(subpath, directory) {
         Some(res) => res,
         _ => {
-            send_error(stream, status::NOT_FOUND);
-            return;
+            return None;
         }
     };
-    if !path.is_file().await {
-        send_error(stream, status::NOT_FOUND);
-        return;
+    if !path.is_file() {
+        return None;
     }
-    debug!("Client requested file dump for file: {}", &content);
-    let mut payload = status::SUCCESS.to_le_bytes().to_vec();
-    payload.extend_from_slice(SERVER_PTCLS[1]);
-    let filedump = File::open(&path);
-    match filedump {
-        Ok(mut file) => {
-            let mut buffer = Vec::new();
-            match file.read_to_end(&mut buffer) {
-                Ok(_) => {
-                    payload.extend_from_slice(&buffer);
-                }
-                Err(e) => {
-                    warn!("Failed to read file: {}", e);
-                    send_error(stream, status::NOT_FOUND);
-                    return;
-                }
-            }
-        }
-        Err(e) => {
-            warn!("Failed to open file: {}", e);
-            send_error(stream, status::NOT_FOUND);
-            return;
-        }
-    }
-    send_data(&payload, stream);
+    Some(path)
 }
 
 fn pathcheck(subpath_str: &str, origpath: &Path) -> Option<PathBuf> {
